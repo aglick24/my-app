51c3ece718a048ef87ebf8a91af98dcad4995611
Alex Glick <alg100@duke.edu>
2023-03-10 14:41:11 -0500

    changed about a bit more

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
                         <h4>Alex Glick</h4>
                         <p>I am from Manhasset, NY, and I am now a senior at Duke, working towards a B.S. dual-major in Computer Science and Public Policy.</p>
                         <p>In high school, I became involved in politics and worked as an intern on a congressional campaign that flipped NY-11 blue in 2018. By my sophomore year of college, my interest grew, and I made CS my first major.</p>
-                        <p>With a winding career path. I have been able to succeed in non-traditional routes due to my ability to easily pick up new skills, acquire leadership roles while in school, interdisciplinary avenue of study, and willingness to collaborate with coworkers. Through this work, I am proficient in Java, Python, Typescript, ReactJS, SQL, and more with experience in machine learning, while also being able to clearly communicate complex ideas.</p>
+                        <p>With a winding career path. I have been able to succeed in non-traditional routes due to my ability to easily pick up new skills, acquire leadership roles while in school, interdisciplinary avenue of study, and willingness to collaborate with coworkers. Through this work, I am proficient in Java, Python, Typescript, ReactJS, SQL, and more with experience in machine learning, while also being able to clearly communicate the complex ideas on which I work.</p>
                     </div>
                 </div>
             </div>
